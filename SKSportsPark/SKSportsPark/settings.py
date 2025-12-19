0e64a095f4227b867541d29937eb6ce6e9d33331
mallu201 <telabakkanavarmallu@gmail.com>
2025-12-19 18:55:31 +0530

    Comment out old DATABASES configuration
    Comment out the old DATABASES configuration in settings.py.

--- SKSportsPark/SKSportsPark/settings.py
+++ SKSportsPark/SKSportsPark/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'SKSportsPark.wsgi.application'
       # 'default': {
           # 'ENGINE': 'django.db.backends.sqlite3',
           # 'NAME': BASE_DIR / 'db.sqlite3',
-      }
+      # }
  # }
 
 
