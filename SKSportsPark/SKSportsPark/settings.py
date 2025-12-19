6f44c824e39893272a7d12f8a210d9ef8d3f19b8
mallu201 <telabakkanavarmallu@gmail.com>
2025-12-19 14:00:55 +0530

    Update settings.py

--- SKSportsPark/SKSportsPark/settings.py
+++ SKSportsPark/SKSportsPark/settings.py
@@ -76,15 +76,23 @@ WSGI_APPLICATION = 'SKSportsPark.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'sk_turf33',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Admin@123',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
+    
+    # }
+# }
+
+# Use SQLite for now (easier for deployment)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sk_turf33',
-        'USER': 'root',
-        'PASSWORD': 'Admin@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
